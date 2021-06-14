e193dfb118c47a721386e57c2990549023e7f400
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2021-06-15 00:04:59 +0530

    Completed CMake config and added a basic hello world program to test build setup

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <chrono>
+#include <algorithm>
+#include <string>
+
+#include <cadmium/modeling/coupling.hpp>
+#include <cadmium/modeling/ports.hpp>
+#include <cadmium/modeling/dynamic_model_translator.hpp>
+#include <cadmium/concept/coupled_model_assert.hpp>
+#include <cadmium/modeling/dynamic_coupled.hpp>
+#include <cadmium/modeling/dynamic_atomic.hpp>
+#include <cadmium/engine/pdevs_dynamic_runner.hpp>
+#include <cadmium/logger/tuple_to_ostream.hpp>
+#include <cadmium/logger/common_loggers.hpp>
+#include <cadmium/io/iestream.hpp>
+
+int main()
+{
+    std::cout << "Hello world\n";
+
+    return 0;
+}
