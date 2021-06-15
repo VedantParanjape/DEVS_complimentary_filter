9214a7f3484b5c9443c1931fb85930bfc005fa56
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2021-06-15 23:23:02 +0530

    Completed atomic model for acclerometer and gyroscope

--- include/atomic_model/accelerometer.hpp
+++ include/atomic_model/accelerometer.hpp
@@ -12,7 +12,13 @@
 using namespace cadmium;
 using namespace std;
 
-/***** (1) *****/
+// dummy accelerometer read function
+std::vector<float> read_accelerometer()
+{
+    std::vector<float> ret_val = {1.0, 2.0, 3.0};
+    return ret_val;
+}
+
 //Port definition
 struct accelerometer_ports
 {
@@ -21,72 +27,43 @@ struct accelerometer_ports
     };
 };
 
-/***** (2) *****/
 template <typename TIME>
 class accelerometer
 {
+private:
+    TIME refresh_rate;
+
 public:
     using output_ports = tuple<typename accelerometer_ports::out>;
-
+    
     // default constructor
-    accelerometer()
+    accelerometer() noexcept
     {
+        refresh_rate = TIME("00:00:00:100");
     }
 
-    // external transition
-    void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs)
+    // parameterized constructor
+    accelerometer(TIME refresh_rate_sensor)
     {
-        vector<Message_t> bag_port_in;
-        bag_port_in = get_messages<typename Subnet_defs::in>(mbs);
-        state.index++;
-        if ((double)rand() / (double)RAND_MAX < 0.95))
-            {
-                state.packet = bag_port_in[0];
-                state.transmitting = true;
-            }
-        else
-        {
-            state.transmitting = false;
-        }
-    }
-
-    // confluent transition
-    void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs)
-    {
-        internal_transition();
-        external_transition(TIME(), move(mbs));
+        refresh_rate = refresh_rate_sensor;
     }
 
     // output function
     typename make_message_bags<output_ports>::type output() const
     {
         typename make_message_bags<output_ports>::type bags;
-        vector<Message_t> bag_port_out;
-        bag_port_out.push_back(state.packet);
-        get_messages<typename Subnet_defs::out>(bags) = bag_port_out;
-    }
-    return bags;
+        vector<std::vector<float>> bag_port_out;
 
-    // time_advance function
+        bag_port_out.push_back(read_accelerometer());
+        get_messages<typename accelerometer_ports::out>(bags) = bag_port_out;
+        
+        return bags;
+    }
+
+    // time advance function
     TIME time_advance() const
     {
-        TIME next_internal;
-        if (state.transmitting)
-        {
-            next_internal = TIME("00:00:03:000");
-        }
-        else
-        {
-            next_internal = numeric_limits<TIME>::infinity();
-        }
-        return next_internal;
-    }
-
-    friend ostringstream &operator<<(ostringstream &os,
-                                     const typename Subnet<TIME>::state_type &i)
-    {
-        os << "index: " << i.index << " & transmitting: " << i.transmitting;
-        return os;
+        return refresh_rate;
     }
 };
 
--- include/atomic_model/gyroscope.hpp
+++ include/atomic_model/gyroscope.hpp
@@ -1,4 +1,70 @@
 #ifndef __GYROSCOPE_HPP__
 #define __GYROSCOPE_HPP__
 
+#include <cadmium/modeling/ports.hpp>
+#include <cadmium/modeling/message_bag.hpp>
+
+#include <assert.h>
+#include <string>
+#include <random>
+#include <limits>
+
+using namespace cadmium;
+using namespace std;
+
+// dummy accelerometer read function
+std::vector<float> read_gyroscope()
+{
+    std::vector<float> ret_val = {1.0, 2.0, 3.0};
+    return ret_val;
+}
+
+//Port definition
+struct gyroscope_ports
+{
+    struct out : public out_port<std::vector<float>>
+    {
+    };
+};
+
+template <typename TIME>
+class gyroscope
+{
+private:
+    TIME refresh_rate;
+
+public:
+    using output_ports = tuple<typename gyroscope_ports::out>;
+    
+    // default constructor
+    gyroscope() noexcept
+    {
+        refresh_rate = TIME("00:00:00:100");
+    }
+
+    // parameterized constructor
+    gyroscope(TIME refresh_rate_sensor)
+    {
+        refresh_rate = refresh_rate_sensor;
+    }
+
+    // output function
+    typename make_message_bags<output_ports>::type output() const
+    {
+        typename make_message_bags<output_ports>::type bags;
+        vector<std::vector<float>> bag_port_out;
+
+        bag_port_out.push_back(read_gyroscope());
+        get_messages<typename gyroscope_ports::out>(bags) = bag_port_out;
+        
+        return bags;
+    }
+
+    // time advance function
+    TIME time_advance() const
+    {
+        return refresh_rate;
+    }
+};
+
 #endif
