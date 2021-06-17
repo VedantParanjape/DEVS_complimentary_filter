f6d0245595364c4d588bff23b63bb00834fcf5d9
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2021-06-17 20:19:28 +0530

    Added fusion controller model and complimentary filter model

--- include/algorithm/complimentary_filter.hpp
+++ include/algorithm/complimentary_filter.hpp
@@ -1,4 +1,55 @@
 #ifndef __COMPLIMENTARY_FILTER_HPP__
 #define __COMPLIMENTARY_FILTER_HPP__
 
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+const double rad_to_degree_coeff = 57.2957795;
+static uint64_t prev_time = 0;
+
+uint64_t micros()
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+std::vector<float> complimentary_filter(std::vector<float> accel_raw, std::vector<float> gyro_raw, std::vector<float> imu_offset)
+{
+    std::vector<float> accel_angles;
+    std::vector<float> gyro_angles;
+    static std::vector<float> complimentary_angles;
+    const float alpha = 0.8;
+
+    accel_angles[0] = (atan2(accel_raw[0], sqrt(pow(accel_raw[1], 2) + pow(accel_raw[2], 2))) * rad_to_degree_coeff) - imu_offset[0];
+    accel_angles[1] = (atan2(accel_raw[1], sqrt(pow(accel_raw[2], 2) + pow(accel_raw[0], 2))) * rad_to_degree_coeff) - imu_offset[1];
+    accel_angles[2] = (atan2(accel_raw[2], sqrt(pow(accel_raw[0], 2) + pow(accel_raw[1], 2))) * rad_to_degree_coeff) - imu_offset[2];
+
+    if (prev_time == 0)
+    {
+        complimentary_angles[0] = accel_angles[0];
+        complimentary_angles[1] = accel_angles[1];
+        complimentary_angles[2] = accel_angles[2];
+
+        prev_time = micros();
+
+        return complimentary_angles;
+    }
+
+    uint64_t curr_time = micros();
+    float dt = (float)(curr_time - prev_time) / 1000000;
+    prev_time = curr_time;
+
+    gyro_angles[0] = complimentary_angles[0] + gyro_raw[0] * dt;
+    gyro_angles[1] = complimentary_angles[1] + gyro_raw[1] * dt;
+    gyro_angles[2] = complimentary_angles[2] + gyro_raw[2] * dt;
+
+    complimentary_angles[0] = alpha * gyro_angles[0] + (1 - alpha) * accel_angles[0];
+    complimentary_angles[1] = alpha * gyro_angles[1] + (1 - alpha) * accel_angles[1];
+    complimentary_angles[2] = alpha * gyro_angles[2] + (1 - alpha) * accel_angles[2];
+
+    return complimentary_angles;
+}
+
 #endif
--- include/atomic_model/fusion_controller.hpp
+++ include/atomic_model/fusion_controller.hpp
@@ -1,4 +1,91 @@
 #ifndef __FUSION_CONTROLLER_HPP__
 #define __FUSION_CONTROLLER_HPP__
 
+#include <cadmium/modeling/ports.hpp>
+#include <cadmium/modeling/message_bag.hpp>
+#include <algorithm/complimentary_filter.hpp>
+
+#include <assert.h>
+#include <string>
+#include <random>
+#include <limits>
+
+using namespace cadmium;
+using namespace std;
+
+struct fusion_controller_ports
+{
+    struct in_accel : public in_port<std::vector<float>> {};
+    struct in_gyro : public in_port<std::vector<float>> {};
+    struct in_offset: public in_port<std::vector<float>> {};
+
+    struct out_fused_angle : public out_port<std::vector<float>> {};
+};
+
+template <typename TIME>
+class fusion_controller
+{
+public:
+    using input_ports = tuple<typename fusion_controller_ports::in_accel, typename fusion_controller_ports::in_gyro>;
+    using output_ports = tuple<typename fusion_controller_ports::out_fused_angle>;
+
+    fusion_controller() noexcept
+    {
+        state.active = false;
+    }
+
+    struct state_type
+    {
+        std::vector<float> fused_angle;
+        bool active;
+    };
+    state_type state;
+
+    // internal transition function
+    void internal_transition()
+    {
+        state.active = false;
+    }
+
+    // external transition function
+    void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs)
+    {
+        state.fused_angle = complimentary_filter(get_messages<typename fusion_controller_ports::in_accel>(mbs)[0],
+                                                    get_messages<typename fusion_controller_ports::in_gyro>(mbs)[0],
+                                                       get_messages<typename fusion_controller_ports::in_offset>(mbs)[0]);
+
+        state.active = true;
+    }
+
+    void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs)
+    {
+        internal_transition();
+        external_transition(TIME(), std::move(mbs));
+    }
+
+    // output function
+    typename make_message_bags<output_ports>::type output() const 
+    {
+        typename make_message_bags<output_ports>::type bags;
+
+        get_messages<typename fusion_controller_ports::out_fused_angle>(bags).push_back(state.fused_angle);
+
+        return bags;
+    }
+
+    // time_advance function
+    TIME time_advance() const 
+    {
+        // + TODO: what should be time here ?
+        return state.active ? TIME("00:00:00") : std::numeric_limits<TIME>::infinity();
+    }
+
+    friend ostringstream& operator<<(ostringstream& os, const typename fusion_controller<TIME>::state_type& i) 
+    {
+        os << "fused angle:" << " x: " << i.fused_angle[0] << " y: " << i.fused_angle[1] << " z: " << i.fused_angle[2] << "\n";
+        
+        return os;
+    }
+};
+
 #endif
