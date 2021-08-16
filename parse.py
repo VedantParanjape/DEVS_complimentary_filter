82ed412fd79bf8a65763cb3f2e294aa64034f73c
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2021-08-17 01:46:54 +0530

    Updated script to plot and changed time steps in input data

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 import time
 
-infile = open("mpu_reading.txt")
+infile = open("top_model/inputs/mpu_reading.txt")
 outfile_a_x = open("top_model/inputs/accelerometer_x.txt", "w+")
 outfile_a_y = open("top_model/inputs/accelerometer_y.txt", "w+")
 outfile_a_z = open("top_model/inputs/accelerometer_z.txt", "w+")
@@ -20,17 +20,17 @@ for i in infile.readlines():
 
 time_counter = 0
 for i in accel_readings:
-    outfile_a_x.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[0])+"\n")
-    outfile_a_y.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[1])+"\n")
-    outfile_a_z.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[2])+"\n")
-    time_counter = time_counter + 100
+    outfile_a_x.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[0])+"\n")
+    outfile_a_y.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[1])+"\n")
+    outfile_a_z.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[2])+"\n")
+    time_counter = time_counter + 10
 
 time_counter = 0
 for i in gyro_readings:
-    outfile_g_x.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[0])+"\n")
-    outfile_g_y.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[1])+"\n")
-    outfile_g_z.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{int((time_counter%1000)/100)}00 " + str(i[2])+"\n")
-    time_counter = time_counter + 100
+    outfile_g_x.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[0])+"\n")
+    outfile_g_y.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[1])+"\n")
+    outfile_g_z.write(time.strftime("%H:%M:%S", time.gmtime(time_counter/1000)) + f":{time_counter%1000:03} " + str(i[2])+"\n")
+    time_counter = time_counter + 10
 
 print(len(accel_readings))
 print(len(gyro_readings))
--- plot_output.py
+++ plot_output.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import openpyxl
 from xlsxwriter import worksheet
 import math
+import numpy as np
 
 workbook = openpyxl.load_workbook(filename="top_model/outputs/simulation_data.xlsx", data_only=True)
 sheet = workbook.active
@@ -19,6 +20,7 @@ fused_angle_z = []
 g_x = 0
 g_y = 0
 g_z = 0
+counter = 0
 for row in sheet.iter_rows(min_row=2, values_only=True):
     time.append(row[0])
     Ax = float(row[1])
@@ -27,22 +29,61 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     a_x = math.atan2(Ax, (Ay**2 + Az**2)**0.5)*180/math.pi
     a_y = math.atan2(Ay, (Ax**2 + Az**2)**0.5)*180/math.pi
     a_z = math.atan2(Az, (Ay**2 + Ax**2)**0.5)*180/math.pi
-    g_x = g_x + float(row[4])*0.1
-    g_y = g_y + float(row[4])*0.1
-    g_z = g_z + float(row[4])*0.1
+    if counter == 0:
+        g_x = a_x
+        g_y = a_y
+        g_z = a_z
+    else:
+        g_x = g_x + float(row[4])*0.01
+        g_y = g_y + float(row[4])*0.01
+        g_z = g_z + float(row[4])*0.01
     accel_x.append(a_x)
     accel_y.append(a_y)
     accel_z.append(a_z)
     gyro_x.append(g_x)
     gyro_y.append(g_y)
     gyro_z.append(g_z)
-    fused_angle_x.append(row[7])
-    fused_angle_y.append(row[8])
-    fused_angle_z.append(row[9])
+    fused_angle_x.append(float(row[7]))
+    fused_angle_y.append(float(row[8]))
+    fused_angle_z.append(float(row[9]))
+    counter = counter + 1
 
-plt.ylim(-1000, 1000)
-plt.plot(accel_z, label="acceleration_angle (X)")
-plt.plot(gyro_z, label="gyroscope_angle (X)")
-plt.plot(fused_angle_z, label="fusion angle (X)")
-plt.legend(loc="upper left")
+plt.plot(time, accel_x, label="acceleration_angle (X)")
+plt.plot(time, gyro_x, label="gyroscope_angle (X)")
+plt.plot(time, fused_angle_x, label="fusion angle (X)")
+plt.xticks([time[i] for i in range(0, len(time), 40)])
+plt.xlabel("Time")
+plt.ylabel("Angle")
+plt.legend(fancybox=True)
+
+fig, ax = plt.subplots(nrows=3, ncols=1)
+ax[0].plot(time, accel_x, label="acceleration_angle (X)")
+ax[0].plot(time, gyro_x, label="gyroscope_angle (X)")
+ax[0].plot(time, fused_angle_x, label="fusion angle (X)")
+ax[0].set_xticks([time[i] for i in range(0, len(time), 40)])
+ax[0].set_xlabel("Time")
+ax[0].set_ylabel("Angle")
+ax[0].legend(fancybox=True)
+ax[0].set_title("X Axis")
+
+ax[1].plot(time, accel_y, label="acceleration_angle (Y)")
+ax[1].plot(time, gyro_y, label="gyroscope_angle (Y)")
+ax[1].plot(time, fused_angle_y, label="fusion angle (Y)")
+ax[1].set_xticks([time[i] for i in range(0, len(time), 40)])
+ax[1].set_xlabel("Time")
+ax[1].set_ylabel("Angle")
+ax[1].legend(fancybox=True)
+ax[1].set_title("Y Axis")
+
+ax[2].plot(time, accel_z, label="acceleration_angle (Z)")
+ax[2].plot(time, gyro_z, label="gyroscope_angle (Z)")
+ax[2].plot(time, fused_angle_z, label="fusion angle (Z)")
+ax[2].set_xticks([time[i] for i in range(0, len(time), 40)])
+ax[2].set_xlabel("Time")
+ax[2].set_ylabel("Angle")
+ax[2].legend(fancybox=True)
+ax[2].set_title("Z Axis")
+
+fig.tight_layout()
 plt.show()
+
